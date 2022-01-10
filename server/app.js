8eaeefd80c2a222e6e24289274d86912a898785e
Hrishikesh S <hrish2006@gmail.com>
2022-01-09 19:40:42 -0800

    Initial commit for the server.

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(4000, () => {
+    console.log("Server started. Listening to requests on port 4000");
+})
+
+
+app.get('/', (req, res) => {
+  res.end('Hello, world');
+})
