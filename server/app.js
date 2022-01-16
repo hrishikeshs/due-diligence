f811f60ae39e795d24aae6765bbb7fce9ee20c34
Hrishikesh S <hrish2006@gmail.com>
2022-01-15 21:07:55 -0800

    Add deployment to aws workflow, clean up package.json files.

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log("Server started. Listening to requests on port 4000");
 })
 
