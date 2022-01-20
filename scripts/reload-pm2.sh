7ee0948703d212318f3e7809514d05fd41a60d6e
Hrishikesh S <hrish2006@gmail.com>
2022-01-19 20:50:03 -0800

    Modify pm2-script for red hat enterprise linux

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,20 +1,26 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-cd /var/www/sites-available/due-diligence
-mkdir dist
-cd client/due-diligence-web
+sudo cd /var/www/sites-available/due-diligence
+sudo mkdir dist
+sudo cd client/due-diligence-web
 
 #install node js
-sudo curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
-sudo /usr/bin/bash n lts
-sudo npm install -g n
-# Install the correct version of node.
-sudo n 12.12.0
+sudo dnf module install nodejs:12
+
 sudo npm install -g yarn
+
+sudo cd /var/www/sites-available/due-diligence/client
+
 sudo yarn install
 CI=true sudo yarn build
-cd /var/www/sites-available/due-diligence/
-cp -r client/due-diligence-web/build dist
-cd /var/www/sites-available/due-diligence/server
-sudo npm install
-pm2 startOrReload ecosystem.config.js
+
+sudo cd /var/www/sites-available/due-diligence/
+
+sudo cp -r client/due-diligence-web/build dist
+
+sudo cd /var/www/sites-available/due-diligence/server
+sudo yarn install
+
+sudo cd /var/www/sites-available/due-diligence
+
+sudo pm2 startOrReload server/ecosystem.config.js
