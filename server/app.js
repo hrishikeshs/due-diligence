d9ce6249654d95e551544006c6eba0dcaac7b709
Hrishikesh S <hrish2006@gmail.com>
2022-01-19 21:13:51 -0800

    Fixes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server started. Listening to requests on port 4000");
+  console.log("Server started. Listening to requests on port 3000");
 })
 
 
