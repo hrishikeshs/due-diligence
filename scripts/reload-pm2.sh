0b527105aefff33b42a0e5af3daa54a52ad7b83b
Hrishikesh S <hrish2006@gmail.com>
2022-01-20 20:00:24 -0800

    Fixes

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-echo "Script executed from: ${PWD}"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-BASEDIR=$(dirname $0)
-echo "Script location: ${BASEDIR}"
+#install node js
+curl -sL https://rpm.nodesource.com/setup_16.x |  -E bash -
 
+yum install -y nodejs
 
-# #install node js
-# curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+export PATH="/usr/bin/node:$PATH"
 
-# sudo yum install -y nodejs
+cd $DIR/client/due-diligence-web
 
-# export PATH="/usr/bin/node:$PATH"
+npm install -g yarn
 
-# sudo cd /var/www/sites-available/due-diligence/client/due-diligence-web && sudo npm install -g yarn
+/usr/local/bin/yarn install
 
-# sudo echo "Current directory is -------------------" && sudo echo $pwd
+CI=true /usr/local/bin/yarn build
 
-# sudo cd /var/www/sites-available/due-diligence/client/due-diligence-web && /usr/local/bin/yarn install
+cp -r $DIR/client/due-diligence-web/build .
 
-# sudo cd /var/www/sites-available/due-diligence/client/due-diligence-web && CI=true sudo /usr/local/bin/yarn build
+cd $DIR/server
 
-# sudo cd /var/www/sites-available/due-diligence/
+npm install --force
 
-# sudo cp -r client/due-diligence-web/build .
-
-# sudo cd /var/www/sites-available/due-diligence/server
-
-# sudo cd /var/www/sites-available/due-diligence/server && sudo npm install --force
-
-# sudo cd /var/www/sites-available/due-diligence/server && sudo node app.js
+node app.js
