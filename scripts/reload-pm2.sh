99359c5d3ebd80d2a385b5be4277cbef0007d1ac
Hrishikesh S <hrish2006@gmail.com>
2022-01-20 18:37:52 -0800

    Fixes.

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -9,9 +9,10 @@ sudo dnf module install nodejs:12
 
 sudo npm install -g yarn
 
-sudo cd /var/www/sites-available/due-diligence/client
+sudo cd /var/www/sites-available/due-diligence/client/due-diligence-web/
 
 sudo /usr/local/bin/yarn install
+
 CI=true sudo /usr/local/bin/yarn build
 
 sudo cd /var/www/sites-available/due-diligence/
@@ -21,6 +22,7 @@ sudo mkdir dist
 sudo cp -r client/due-diligence-web/build dist
 
 sudo cd /var/www/sites-available/due-diligence/server
+
 sudo npm install
 
 sudo node app.js
