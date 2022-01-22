b3d7a2683f2a4dda844aba7e96ab326498e38f49
Hrishikesh S <hrish2006@gmail.com>
2022-01-21 20:25:21 -0800

    Simplify the installation

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#install node js
+curl -sL https://rpm.nodesource.com/setup_16.x |  -E bash -
+
+yum install -y nodejs
+
+npm install -g yarn
+
+export PATH="/usr/bin/node:$PATH"
+
+cd /var/www/sites-available/due-diligence
+
 exec >>/var/log/after-install.log 2>&1
-
-cd client/due-diligence-web
-
-/usr/local/bin/yarn install
-
-CI=true /usr/local/bin/yarn build
-
-cd ../../
 
 cp -r client/due-diligence-web/build .
 
